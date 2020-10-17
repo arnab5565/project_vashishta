6c74eabb1272fb6f8199d4f84113aec840c27543
arnab5565 <arnabmaiti9381@gmail.com>
2020-10-17 16:39:00 +0530

    jkdjkv

--- .vscode/Demo.java
+++ .vscode/Demo.java
@@ -1,6 +1,17 @@
 
 import java.applet.Applet;
-
+import java.awt.*;
 public class Demo extends Applet{
+
+    public void paint(Graphics g){
+
+        setBackground(Color.ORANGE);
+        g.setFont(new Font("TimesNewRoman", 1 , 18));
+        g.drawString("THIS IS AN APPLET", 10 , 100);
+        
+
+
+    }
+
 
 }
