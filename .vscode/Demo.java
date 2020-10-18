5443c1312e1825dbdba01dfb0ccc56e7827c93b6
arnab5565 <arnabmaiti9381@gmail.com>
2020-10-18 15:33:55 +0530

    sdvh

--- .vscode/Demo.java
+++ .vscode/Demo.java
@@ -31,7 +31,7 @@ public class Demo extends Applet{
     public void print(int[] arr){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
-            
+
         }
     }
 
@@ -39,5 +39,28 @@ public class Demo extends Applet{
     public static void main(String[] args){
 
         int[] arr={3,5,2,9,4};
+        sort(arr);
+        print(arr);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
