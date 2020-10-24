f6095898df823608c517312c510517f10fc2d7b9
arnab5565 <arnabmaiti9381@gmail.com>
2020-10-24 17:36:21 +0530

    jksjsdjjshjkf

--- Webmarket/main.js
+++ Webmarket/main.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var am;
+//this must be the position
 am = "arnab";
 var Color;
 (function (Color) {
@@ -22,10 +23,21 @@ console.log(random);
 var list2 = [3, 7, 8, 4, 9, 2, 90, 43];
 console.log(list2);
 console.log(am);
+var list1;
+list1 = [3, 5, 2, 56, 4, 3, 5, 4, 3, 5, 5, 6, 6, 3, 2, 2, 32, 3, 4, 5, 5, 6, 6, 6, 6, 6, 5, 54, 4, 3, 5, 5, 6, 6, 7, 6, 6, 4, 4, 3, 4, 5, 5, 4];
+var list_add = list1 + list2;
+console.log(list1);
+console.log(list_add);
+/**for(list1 in list1){
+
+
+    console.log(list1);
+}
+*/
 /**function add(num1:number, num2:number) {
 
     return num1+num2;
-    
+
 }
 
 add(9,9);
--- Webmarket/main.ts
+++ Webmarket/main.ts
@@ -2,35 +2,46 @@ export { }
 
 let am: string;
 //this must be the position
-  
-
 am = "arnab";
-
-
 enum Color { Red, Green, Brown, White };
-
 let c: Color = Color.Brown;
-
 console.log("letus see what is there in the enum {{c}}");
 let random: any;
-random=true;
+random = true;
 console.log(random);
-random="hello";
+random = "hello";
 console.log(random);
-random=[4,94,84,25,75,73,95,1,74,22,55,33,99,0,3,860,24];
+random = [4, 94, 84, 25, 75, 73, 95, 1, 74, 22, 55, 33, 99, 0, 3, 860, 24];
 console.log(random);
-
 let list2: Array<number> = [3, 7, 8, 4, 9, 2, 90, 43];
-
 console.log(list2);
 console.log(am);
+
+let list1: any;
+list1 = [3, 5, 2, 56, 4, 3, 5, 4, 3, 5, 5, 6, 6, 3, 2, 2, 32, 3, 4, 5, 5, 6, 6, 6, 6, 6, 5, 54, 4, 3, 5, 5, 6, 6, 7, 6, 6, 4, 4, 3, 4, 5, 5, 4];
+
+let list_add: any = list1 + list2;
+
+console.log(list1);
+console.log(list_add);
+
+
+/**for(list1 in list1){
+
+
+    console.log(list1);
+}
+*/
+
+
+
 
 
 
 /**function add(num1:number, num2:number) {
 
     return num1+num2;
-    
+
 }
 
 add(9,9);
