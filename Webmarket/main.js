54667d840b8c9aac8b7c70038b7fb3ca5f9c4cc6
arnab5565 <arnabmaiti9381@gmail.com>
2020-10-24 15:20:49 +0530

    lkk,scn

--- Webmarket/main.js
+++ Webmarket/main.js
@@ -1,3 +1,24 @@
+"use strict";
+exports.__esModule = true;
 var am;
 am = "arnab";
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Brown"] = 2] = "Brown";
+    Color[Color["White"] = 3] = "White";
+})(Color || (Color = {}));
+;
+var c = Color.Brown;
+console.log("letus see what is there in the enum {{c}}");
+var random;
+random = true;
+console.log(random);
+random = "hello";
+console.log(random);
+random = [4, 94, 84, 25, 75, 73, 95, 1, 74, 22, 55, 33, 99, 0, 3, 860, 24];
+console.log(random);
+var list2 = [3, 7, 8, 4, 9, 2, 90, 43];
+console.log(list2);
 console.log(am);
--- Webmarket/main.ts
+++ Webmarket/main.ts
@@ -1,10 +1,23 @@
-export {}
+export { }
 
 let am: string;
-am="arnab";
-
-let list2: Array<number> = [3,7,8,4,9,2,90,43];
+am = "arnab";
 
 
+enum Color { Red, Green, Brown, White };
 
+let c: Color = Color.Brown;
+
+console.log("letus see what is there in the enum {{c}}");
+let random: any;
+random=true;
+console.log(random);
+random="hello";
+console.log(random);
+random=[4,94,84,25,75,73,95,1,74,22,55,33,99,0,3,860,24];
+console.log(random);
+
+let list2: Array<number> = [3, 7, 8, 4, 9, 2, 90, 43];
+
+console.log(list2);
 console.log(am);
